7ef3211d42905a01d67c02107c23c2bdc2a259dd
erich-m <erich.m.maclean@gmail.com>
2022-01-06 16:06:39 -0500

    added user side change log and fixed window resizing bug in main

--- main.js
+++ main.js
@@ -41,6 +41,18 @@ let notes = [261.6256,293.6648,329.6276,349.2282,391.9954,440.0000,493.8833,523.
 
 let version;
 
+let wwTh;
+let wwSx;
+let whHf;
+let xValues;
+let yValues;
+let wValues;
+let hValues;
+let rValue;
+let margins;
+let buttonColors;
+let buttonOffsets;
+
 function preload(){
 	gameFont = loadFont('assets/font.ttf');
 }
@@ -48,21 +60,18 @@ function preload(){
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
-	currentWidth = windowWidth;
-	currentHeight = windowHeight;
-
-	const wwTh = width/3;
-	const wwSx = width/6;
-	const whHf = height/2;
+	wwTh = windowWidth/3;
+	wwSx = windowWidth/6;
+	whHf = windowHeight/2;
 	
-	const xValues = [[0,2*wwTh],[0,2*wwTh,0,2*wwTh],[0,wwSx,4*wwSx,5*wwSx,0,wwSx,4*wwSx,5*wwSx]];
-	const yValues = [[0,0],[0,0,whHf,whHf],[0,0,0,0,whHf,whHf,whHf,whHf]];
-	const wValues = [[wwTh,wwTh],[wwTh,wwTh,wwTh,wwTh],[wwSx,wwSx,wwSx,wwSx,wwSx,wwSx,wwSx,wwSx]];
-	const hValues = [[height,height],[whHf,whHf,whHf,whHf],[whHf,whHf,whHf,whHf,whHf,whHf,whHf,whHf]];
-	const rValue = 20;
-	const margins = 20;
-	const buttonColors = [[color('#F52E2E'),color('#5463FF')],[color('#F52E2E'),color('#5463FF'),color('#FFC717'),color('#1F9E40')],[color('#F52E2E'),color('#5463FF'),color('#FF6619'),color('#24D4C4'),color('#FFC717'),color('#1F9E40'),color('#D41CE5'),color('#616161')]];
-	const buttonOffsets = 70;
+	xValues = [[0,2*wwTh],[0,2*wwTh,0,2*wwTh],[0,wwSx,4*wwSx,5*wwSx,0,wwSx,4*wwSx,5*wwSx]];
+	yValues = [[0,0],[0,0,whHf,whHf],[0,0,0,0,whHf,whHf,whHf,whHf]];
+	wValues = [[wwTh,wwTh],[wwTh,wwTh,wwTh,wwTh],[wwSx,wwSx,wwSx,wwSx,wwSx,wwSx,wwSx,wwSx]];
+	hValues = [[height,height],[whHf,whHf,whHf,whHf],[whHf,whHf,whHf,whHf,whHf,whHf,whHf,whHf]];
+	rValue = 20;
+	margins = 20;
+	buttonColors = [[color('#F52E2E'),color('#5463FF')],[color('#F52E2E'),color('#5463FF'),color('#FFC717'),color('#1F9E40')],[color('#F52E2E'),color('#5463FF'),color('#FF6619'),color('#24D4C4'),color('#FFC717'),color('#1F9E40'),color('#D41CE5'),color('#616161')]];
+	buttonOffsets = 70;
 	//setup buttons
 	let buttonCount = 0;
 	for(var b = 0;b < buttons.length;b++){
@@ -76,29 +85,29 @@ function setup() {
 	
 	textFont(gameFont);
 
-	startButton = new MenuButton(wwTh,whHf - height/6,wwTh,height/3,height/6-margins,margins,color('#4A4559'),buttonOffsets/2,8,notes[8],volume,"START",0.4)
+	startButton = new MenuButton(wwTh,whHf - windowHeight/6,wwTh,windowHeight/3,windowHeight/6-margins,margins,color('#4A4559'),buttonOffsets/2,8,notes[8],volume,"START",0.4)
 	startButton.enableButton();
 	startButton.enableUserControl();
 
-	difficultyButton = new MenuButton(wwTh,6*height/9,wwTh,height/9,rValue,margins,color('#4A4559'),buttonOffsets/2,9,notes[9],volume,"DIFFICULTY LEVEL: " + (difficulty+1),0.4);
+	difficultyButton = new MenuButton(wwTh,6*windowHeight/9,wwTh,windowHeight/9,rValue,margins,color('#4A4559'),buttonOffsets/2,9,notes[9],volume,"DIFFICULTY LEVEL: " + (difficulty+1),0.4);
 	difficultyButton.enableButton();
 	difficultyButton.enableUserControl();
 
-	speedButton = new MenuButton(wwTh,7*height/9,wwTh,height/9,rValue,margins,color('#4A4559'),buttonOffsets/2,10,notes[10],volume,"SPEED MULTIPLIER: " + speed,0.4);
+	speedButton = new MenuButton(wwTh,7*windowHeight/9,wwTh,windowHeight/9,rValue,margins,color('#4A4559'),buttonOffsets/2,10,notes[10],volume,"SPEED MULTIPLIER: " + speed,0.4);
 	speedButton.enableButton();
 	speedButton.enableUserControl();
 
-	instructionButton = new MenuButton(wwTh,8*height/9,wwTh,height/9,rValue,margins,color('#4A4559'),buttonOffsets/2,11,notes[11],volume,"HOW TO PLAY & CREDITS",0.4);
+	instructionButton = new MenuButton(wwTh,8*windowHeight/9,wwTh,windowHeight/9,rValue,margins,color('#4A4559'),buttonOffsets/2,11,notes[11],volume,"HOW TO PLAY & CREDITS",0.4);
 	instructionButton.enableButton();
 	instructionButton.enableUserControl();
 
-	invertButton = new MenuButton(wwTh,2*height/3-0.15*height/3,0.15*wwTh,0.15*height/3,0.05*wwTh,0,color('#4A4559'),buttonOffsets/2,12,notes[12],volume,"Invert",0.34);
+	invertButton = new MenuButton(wwTh,2*windowHeight/3-0.15*windowHeight/3,0.15*wwTh,0.15*windowHeight/3,0.05*wwTh,0,color('#4A4559'),buttonOffsets/2,12,notes[12],volume,"Invert",0.34);
 	invertButton.enableButton();
 	invertButton.enableUserControl();
 
 	flashController = new FlashController(speed,difficulty);
 
-	soundButton = new MenuButton(2*wwTh-0.15*wwTh,2*height/3-0.15*height/3,0.15*wwTh,0.15*height/3,0.05*wwTh,0,color('#4A4559'),buttonOffsets/2,12,notes[13],!volume,"Sound",0.34);
+	soundButton = new MenuButton(2*wwTh-0.15*wwTh,2*height/3-0.15*windowHeight/3,0.15*wwTh,0.15*windowHeight/3,0.05*wwTh,0,color('#4A4559'),buttonOffsets/2,12,notes[13],!volume,"Sound",0.34);
 	soundButton.enableButton();
 	soundButton.enableUserControl();
 
@@ -113,7 +122,7 @@ function setup() {
 	}
 
 	version = createElement('h6','Version: Beta 1.0' + join(flashController.getSequence(),'') + ' Optimized for widescreen display');
-	version.position(width-300,height-40);
+	version.position(windowWidth-300,windowHeight-40);
 }
 
 function draw() {
@@ -301,6 +310,8 @@ function updateHeader(){
 		header = 2;
 	}else if(header == 2){
 		header = 3;
+	}else if(header == 3){
+		header = 4;
 	}else{
 		header = 0;
 	}
@@ -320,10 +331,14 @@ function renderHeading(heading){
 				counter++;
 			}
 		}
-	}else{
+	}else if(heading == 3){
 		textAlign(CENTER,CENTER);
 		noStroke();
 		text("MEMORYMASTER\n\ndesigner : Erich MacLean\nPresident of Coding Club\n\nbased off the classic game of SIMON\n\nMade for Isaac\nVice President of coding club",width/2,height/6);
+	}else{
+		textAlign(CENTER,CENTER);
+		noStroke();
+		text("Change Log\nFixed window resizing bug",width/2,height/6);
 	}
 }
 
@@ -344,4 +359,32 @@ function changeVolume(){
 	instructionButton.updateVolume();
 	soundButton.updateVolume();
 	invertButton.updateVolume();
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth,windowHeight);
+	wwTh = windowWidth/3;
+	wwSx = windowWidth/6;
+	whHf = windowHeight/2;
+	
+	xValues = [[0,2*wwTh],[0,2*wwTh,0,2*wwTh],[0,wwSx,4*wwSx,5*wwSx,0,wwSx,4*wwSx,5*wwSx]];
+	yValues = [[0,0],[0,0,whHf,whHf],[0,0,0,0,whHf,whHf,whHf,whHf]];
+	wValues = [[wwTh,wwTh],[wwTh,wwTh,wwTh,wwTh],[wwSx,wwSx,wwSx,wwSx,wwSx,wwSx,wwSx,wwSx]];
+	hValues = [[height,height],[whHf,whHf,whHf,whHf],[whHf,whHf,whHf,whHf,whHf,whHf,whHf,whHf]];
+	rValue = 20;
+	margins = 20;
+
+	for(var uX = 0;uX < buttons.length;uX++){
+		for(var uY = 0;uY < buttons[uX].length;uY++){
+			buttons[uX][uY].update(xValues[uX][uY],yValues[uX][uY],wValues[uX][uY],hValues[uX][uY],rValue,margins);
+		}
+	}
+
+	startButton.update(wwTh,whHf - windowHeight/6,wwTh,windowHeight/3,windowHeight/6-margins,margins);
+	difficultyButton.update(wwTh,6*windowHeight/9,wwTh,windowHeight/9,rValue,margins);
+	speedButton.update(wwTh,7*windowHeight/9,wwTh,windowHeight/9,rValue,margins);
+	instructionButton.update(wwTh,8*windowHeight/9,wwTh,windowHeight/9,rValue,margins);
+	invertButton.update(wwTh,2*windowHeight/3-0.15*windowHeight/3,0.15*wwTh,0.15*windowHeight/3,0.05*wwTh,0);
+	soundButton.update(2*wwTh-0.15*wwTh,2*height/3-0.15*windowHeight/3,0.15*wwTh,0.15*windowHeight/3,0.05*wwTh,0);
+	version.position(windowWidth-300,windowHeight-40);
 }
